5cf8baa03f758f150463b39a5f88ccf2140854b6
Sandeep Juneja <srjuneja@users.noreply.github.com>
2021-03-29 07:31:45 -0400

    Update word2pdf.py

--- python/word2pdf.py
+++ python/word2pdf.py
@@ -6,46 +6,17 @@ from time import strftime
 # import pythoncom
 # import docx2pdf
 
-# Below imports are required for Linux Ubuntu
-
 from utilities import utility
 
 
 #  Method 1 - simply use docx2pdf - works on Windows and MacOS
 def win_docx2pdf(source_file):
-    # location = os.path.dirname(source_file )    #/srv/volume1/data/eds
-    # file_name = os.path.basename(source_file )  #eds_report.csv
-    # file_name_without_ext = os.path.splitext(file_name)
     dest_file = os.path.splitext(source_file)[0] + ".pdf"
     print("source_file = {}, dest_file = {}".format(source_file, dest_file))
     #  Initialization required to avoid errors
-    # pythoncom.CoInitialize()
+    pythoncom.CoInitialize()
     #  Using docx2pdf convert word to pdf
-    # docx2pdf.convert(source_file, dest_file)
-    # Once pdf file is generated, delete the source file
-    utility.background_remove(source_file)
-
-# Method 2 - using Windows DLLs
-#  https://stackoverflow.com/questions/53077204/convert-microsoft-word-document-to-pdf-using-python
-def win_word_pdf(source_file):
-    global word
-
-    dest_file = os.path.splitext(source_file)[0] + ".pdf"
-    print("source_file = {}, dest_file = {}".format(source_file, dest_file))
-    #  Initialization required to avoid errors
-    # pythoncom.CoInitialize()
-
-    try:
-        # word = client.Dispatch("Word.Application")
-        doc = word.Documents.Open(source_file)
-        print (strftime("%H:%M:%S"), " " + source_file + " -> pdf ", os.path.relpath(dest_file))
-        doc.SaveAs(dest_file, FileFormat = 17)
-        doc.Close()
-    except Exception as e:
-        print (e)
-    finally:
-        word.Quit()
-
+    docx2pdf.convert(source_file, dest_file)
     # Once pdf file is generated, delete the source file
     utility.background_remove(source_file)
 
